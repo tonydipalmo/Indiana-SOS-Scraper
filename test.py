1b4e7a82669106eb1e19586d421c480d036143dc
tonydipalmo <tonydipalmo93@gmail.com>
2025-06-04 00:16:30 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
 from indiana import Indiana
 
-
-#search_keywords = ["basement", "handy", "landsc", "plumb", "electric", "remodel", "paint", "clean", "hvac", "window", "constr"]
-#search_keywords = ["clean", "hvac", "window", "constr"]
-#search_keywords = ["inc", "ind", "llc"]
-search_keywords = ["ave", "dr.", "way", "street", "lane", "court", "road", "rd."]
-#search_keywords = ["renovat", "remodel", "build", "improve", "repair", "bathroom", "kitchen", "basement", "contract", "general"]
-#search_keywords = ["bathroom", "kitchen", "basement", "contract", "general"]
+search_keywords = ["inc", "ind", "llc"]
 
 def test():
     for search in search_keywords:
